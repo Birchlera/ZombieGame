2595fb2b9dfd6d888ef1c5d9006225ce78cd1b3d
Birchlera <albirchler@hotmail.com>
2016-01-29 23:29:15 -0500

    Zombie walking animation and follow
    Still has problems with zombie floating upward

---
+++ Temporary/Assets/zombieWalk.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombieWalk : MonoBehaviour {
+    protected Animator myAnimation;
+	// Use this for initialization
+	void Start () {
+        myAnimation = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myAnimation.SetFloat("speed", Input.GetAxis("Vertical"));
+	}
+}
